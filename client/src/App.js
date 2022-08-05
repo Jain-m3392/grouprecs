de8f3001f37af4d30995536024210d5bd347844d
Mudit Jain <Jain.m3392@gmail.com>
2022-08-05 19:55:15 -0400

    Added .gitignore and replaced boilerplate react code

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Grouprecs
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          Grouprecs
+        </header>
+      </div>
+    </Router>
   );
 }
 
