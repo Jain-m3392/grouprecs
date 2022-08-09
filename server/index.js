3a07c94e24fbc1f69598fd53635436d73c2ab0a5
Mudit Jain <Jain.m3392@gmail.com>
2022-08-08 20:11:32 -0400

    Changed to work with MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const PORT = 4000;
-
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
 });
