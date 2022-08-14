fcb021ed4fd265bba8645d80b3a4d9271facce3c
Mudit Jain <Jain.m3392@gmail.com>
2022-08-14 15:14:23 -0400

    Removed router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          Grouprecs
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">Grouprecs</header>
+    </div>
   );
 }
 
