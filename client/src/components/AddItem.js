3159529de5f0d759b11b129c00aff27ca796165c
Mudit Jain <Jain.m3392@gmail.com>
2022-08-15 09:16:08 -0400

    Implemented static version

---
+++ client/src/components/AddItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddItem = () => {
+  return (
+    <div className="input-group">
+      <div className="input-group-prepend">
+        <button className="btn btn-outline-primary">Add</button>
+      </div>
+      <input type="text" className="form-control" placeholder="Add an item" />
+    </div>
+  );
+};
