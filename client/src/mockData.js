64149af2289ae064ffbe46d3eef976cddd93f5d6
Mudit Jain <Jain.m3392@gmail.com>
2022-08-15 08:53:43 -0400

    Created mock data

--- client/src/mockData.js
+++ client/src/mockData.js
@@ -1,7 +1,19 @@
 const sampleMyLists = {
-
-}
+  lists: ["Ajv7eIl9", "Jvi53Csm", "8Fv14Kjx", "eq3cO4nC", "mN6JVDei"],
+};
 
 const sampleList = {
-
-}
+  id: "Ajv8eIl9",
+  name: "My list #1",
+  items: [
+    "Science fiction book",
+    "Mystery book",
+    "Action movie",
+    "Jazz album",
+    "Horror movie",
+    "Comedy show",
+    "Rock song",
+    "Pop EP",
+    "Fantasy novel",
+  ],
+};
