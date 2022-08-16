8a998244b40a7db509c65e4fc4e71d8b0ec50d67
Mudit Jain <Jain.m3392@gmail.com>
2022-08-16 09:41:02 -0400

    Implemented static, unstyled version

--- client/src/components/ListMenu.js
+++ client/src/components/ListMenu.js
@@ -1,7 +1,25 @@
 import React from "react";
+import sampleData from "../mockData";
+import ListRow from "./ListRow";
 
 const ListMenu = () => {
-    return()
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>My lists</td>
+          <td>
+            <button className="btn btn-outline-primary">Create List</button>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        {sampleData.sampleMyLists.lists.map((list) => (
+          <ListRow listName={list} />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default ListMenu;
