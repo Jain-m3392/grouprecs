8511cf9f99e2dddc1fe436d16c7341437bff28ef
Mudit Jain <Jain.m3392@gmail.com>
2022-08-16 09:27:30 -0400

    Removed header

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import List from "./components/List";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Grouprecs</header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
