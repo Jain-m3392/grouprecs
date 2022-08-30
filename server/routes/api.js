ce715eb2a364548c1fee31828d8ce17301fbe6a0
Mudit Jain <Jain.m3392@gmail.com>
2022-08-30 17:07:50 -0400

    Added url generator function

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,32 @@ const express = require("express");
 const listModel = require("../models/list");
 const app = express();
 
+//business logic for generating urls
+function generateUrl() {
+  //initialize values
+  let charSet =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let url = "";
+  const createdTime = String(Date.now());
+
+  //generate 1st 3 random characters
+  for (i = 0; i < 3; i++) {
+    url = url.concat(charSet[Math.floor(Math.random() * 61)]);
+  }
+
+  //get next 4 characters based on following double digit numbers in createdTime
+  for (i = 0; i < 4; i++) {
+    let source = createdTime.slice(i * 2 + 1, i * 2 + 3);
+    source = source * Math.floor(Math.random() * 9);
+    url = url.concat(charSet[source % 61]);
+  }
+
+  //get last character based on last digits in createdTime
+  let lastSource = createdTime.slice(11);
+  url = url.concat(charSet[lastSource % 61]);
+  return url;
+}
+
 //get all lists
 /*
 app.get("/", async (req, res) => {
@@ -24,13 +50,13 @@ app.get("/list/:url", async (req, res) => {
   }
 });
 
-//add list
-app.post("/add", async (req, res) => {
+//create list
+app.post("/create", async (req, res) => {
   const list = new listModel(req.body);
-
+  list.url = generateUrl();
   try {
     await list.save();
-    res.send(user);
+    res.send(list.url);
   } catch (error) {
     res.status(500).send(error);
   }
