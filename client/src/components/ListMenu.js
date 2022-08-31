7240681176edc94aeb2418c4f0596fa767b4d7d4
Mudit Jain <Jain.m3392@gmail.com>
2022-08-31 11:33:41 -0400

    Removed unused import statement & changed useEffect function

--- client/src/components/ListMenu.js
+++ client/src/components/ListMenu.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import sampleData from "../mockData";
 import ListRow from "./ListRow";
 import CreateList from "./CreateList";
 
@@ -7,8 +6,15 @@ const ListMenu = () => {
   const [storedLists, setStoredLists] = useState([]);
 
   useEffect(() => {
-    setStoredLists(JSON.parse(localStorage.getItem("lists")));
+    console.log("lists are", localStorage.getItem("lists"));
+    try {
+      setStoredLists(JSON.parse(localStorage.getItem("lists")));
+    } catch (err) {
+      console.log("Storage issue:", err);
+    }
   });
+
+  console.log("State lists are", storedLists);
 
   return (
     <table>
