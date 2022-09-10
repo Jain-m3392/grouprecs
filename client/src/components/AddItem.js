3aeef4845f0ed92041e6f3955b5c65a1e9535c66
Mudit Jain <Jain.m3392@gmail.com>
2022-09-09 20:52:58 -0400

    Added handleAdd function & props

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const AddItem = () => {
+const AddItem = (props) => {
+  const handleAdd = () => {};
+
   return (
     <div className="input-group">
       <div className="input-group-prepend">
-        <button className="btn btn-outline-primary">Add</button>
+        <button className="btn btn-outline-primary" onClick={handleAdd}>
+          Add
+        </button>
       </div>
       <input type="text" className="form-control" placeholder="Add an item" />
     </div>
