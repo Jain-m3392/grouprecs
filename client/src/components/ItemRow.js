d6c6a9021b4f8f27b8fdde3939aeb550cc55e1b6
Mudit Jain <Jain.m3392@gmail.com>
2022-09-09 21:53:49 -0400

    Added ability to delete items

--- client/src/components/ItemRow.js
+++ client/src/components/ItemRow.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ItemRow = (props) => {
+  const handleDelete = () => {
+    //remove item from listItems
+    let newItems = props.list.items.filter((item) => item !== props.item);
+
+    //new object to be used on our API
+    let newList = {
+      url: props.list.url,
+      name: props.list.name,
+      items: newItems,
+    };
+
+    //update server
+    fetch(`http://localhost:5000/update/${props.list.url}`, {
+      method: "PATCH",
+      body: JSON.stringify(newList),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((res) =>
+        res.json().then(() => {
+          //update react page
+          props.setList(newList);
+        })
+      )
+      .then((json) => console.log(json));
+  };
+
   return (
     <tr>
       <td>{props.item}</td>
       <td>
-        <button className="btn btn-outline-danger">Delete</button>
+        <button className="btn btn-outline-danger" onClick={handleDelete}>
+          Delete
+        </button>
       </td>
     </tr>
   );
