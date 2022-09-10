c7e62be6b89ed3f1b04ef55211213fcfbdce478f
Mudit Jain <Jain.m3392@gmail.com>
2022-09-09 21:28:24 -0400

    Added ability to add items to a list

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddItem = (props) => {
-  const handleAdd = () => {};
+  const [itemName, setItemName] = useState("");
+
+  const handleAdd = () => {
+    let newList = {
+      url: props.list.url,
+      name: props.list.name,
+      items: [...props.list.items, itemName],
+    };
+    //update server
+    fetch(`http://localhost:5000/update/${props.list.url}`, {
+      method: "PATCH",
+      body: JSON.stringify(newList),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((res) =>
+        res.json().then(() => {
+          //update react page
+          props.setList(newList);
+          setItemName("");
+        })
+      )
+      .then((json) => console.log(json));
+  };
 
   return (
     <div className="input-group">
@@ -10,7 +32,15 @@ const AddItem = (props) => {
           Add
         </button>
       </div>
-      <input type="text" className="form-control" placeholder="Add an item" />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Add an item"
+        value={itemName}
+        onChange={(e) => {
+          setItemName(e.target.value);
+        }}
+      />
     </div>
   );
 };
